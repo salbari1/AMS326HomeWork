8afb77917f4f5e52523eb611d801df309ca1e363
salbari1 <35353760+salbari1@users.noreply.github.com>
2020-03-27 17:45:51 -0400

    Add files via upload

---
+++ Hw2.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Hw2 {
+	 
+	private static double UniRandom(double min, double max) {
+		double result = Math.random() * (max - min) + min;
+		return result;
+	}
+	
+	static double[][] multiplyMatrix(double[][] firstMatrix, double[][] secondMatrix) {
+	    double[][] result = new double[firstMatrix.length][secondMatrix[0].length];
+	 
+	    for (int row = 0; row < result.length; row++) {
+	        for (int col = 0; col < result[row].length; col++) {
+	            result[row][col] = multiplyMatrixCell(firstMatrix, secondMatrix, row, col);
+	        }
+	    }
+	 
+	    return result;
+	}
+	static double multiplyMatrixCell(double[][] firstMatrix, double[][] secondMatrix, int row, int col) {
+	    double cell = 0;
+	    for (int i = 0; i < secondMatrix.length; i++) {
+	        cell += firstMatrix[row][i] * secondMatrix[i][col];
+	    }
+	    return cell;
+	}
+
+	public static void main(String[] args) {
+		
+		
+	    double[][] matrix3 = new double[(int) Math.pow(2,12)][(int) Math.pow(2,12)];
+
+	    for (int i1 = 0; i1 < 4096; i1++) {
+	        for (int j = 0; j < 4096; j++) {
+	        	matrix3[i1][j] = UniRandom(-1,1);
+	        }
+	    }
+	    double[][] matrix4 = new double[(int) Math.pow(2,12)][(int) Math.pow(2,12)];
+
+	    for (int i1 = 0; i1 < 4096; i1++) {
+	        for (int j = 0; j < 4096; j++) {
+	        	matrix4[i1][j] = UniRandom(-1,1);
+	        }
+	    }
+	    double[][] actual2 = multiplyMatrix(matrix3, matrix4);
+	    
+	    for (int i1 = 0; i1 < actual2.length; i1++) {
+	        for (int j = 0; j < actual2[i1].length; j++) {
+	            System.out.print(actual2[i1][j] + " ");
+	        }
+	    }
+	        
+	        
+	   
+	    }
+	}
+
+
---
+++ prob2.java
@@ -0,0 +1,68 @@
+import java.util.Random;
+
+public class prob2 {
+	
+	public static double RandomNorm (double mean, double sd) {
+		Random r = new Random();
+		double val = (r.nextGaussian() * sd + mean);
+		return val;
+	}
+	
+	static double[] normalmatrix = new double [89];
+	
+
+	static double x;
+	
+	public static void DailyInfection1(double t1){
+		for (int i = 0; i < 89; i++) {
+        	normalmatrix[i] = RandomNorm(0.18,0.08);
+        }
+		
+			
+		for(int a = 1; a < 46; a++) {
+			
+			if(a == 1) {
+				System.out.print("Patients at Day: " + 1 + " is " + 10);
+				x = t1;
+			}
+			else
+			System.out.printf("Patients at Day: " + a + " is " + "%.0f",x);
+			System.out.println("");
+			int random = (int) (Math.random() * 88 + 1);
+			x = x * (1 + normalmatrix[random]);
+		}
+	}
+	public static void DailyInfection2(double t1){
+		for (int i = 0; i < 89; i++) {
+        	normalmatrix[i] = RandomNorm(-0.24,0.04);
+        }
+		
+			
+		for(int a = 46; a < 91; a++) {
+			
+			if(a == 1) {
+				System.out.println("Patients at Day: " + 46 + " is " + 10);
+				x = t1;
+			}
+			else
+			System.out.printf("Patients at Day: " + a + " is " + "%.0f",x);
+			System.out.println("");
+			int random = (int) (Math.random() * 88 + 1);
+			x = x * (1 + normalmatrix[random]);
+		}
+	}
+		
+	
+	
+	
+	public static void main( String args[] ) {
+		
+		DailyInfection1(10);
+		DailyInfection2(x);
+			
+		}
+		
+	
+}
+
+
